214b765c8884d643736048b9e87436545272deba
Ernest Macharia <ernesthuku@gmail.com>
2020-06-20 23:50:57 +0300

    Update settings.py

--- project/chat/chat/settings.py
+++ project/chat/chat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5fvs%tm!no)9mi=tx)_j^18lu2h)msywx5+tm8%6++kbtv+$uy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
